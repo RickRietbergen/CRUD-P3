885828e0299c0dc27cd5bd3153f2c9aede5a5d5b
RickRietbergen <rbps3.2016@gmail.com>
2022-03-14 13:22:08 +0100

    add php
    yesss, header

---
+++ header.php
@@ -0,0 +1,32 @@
+<header>
+  <div class="header">
+    <nav>
+      <li>
+        <a href="#home"
+          ><img
+            class="snackbar__logo"
+            src="Assets/afbeeldingen/snackbarlogo.png"
+            alt="logo__link__to__home"
+        /></a>
+        <ul class="ul1">
+          <a href="#home">home</a>
+        </ul>
+        <ul>
+          <a href="#about">about us</a>
+        </ul>
+        <ul>
+          <a href="#menu">menu</a>
+        </ul>
+        <ul>
+          <a href="#form">formulier</a>
+        </ul>
+        <ul>
+          <a href="#werken">werken bij BigSnack</a>
+        </ul>
+        <ul>
+          <a href="#contact">contact us</a>
+        </ul>
+      </li>
+    </nav>
+  </div>
+</header>
--- index.php
+++ index.php
@@ -13,22 +13,12 @@
     <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap" rel="stylesheet">
   </head>
   <body>
+    <?php
+      include_once("header.php");
+    ?>
     <main>
         <div class="container__homepage" id="home">
-            <header>
-              <div class="header">
-                <nav>
-                  <li>
-                    <a href="#home"><img class="snackbar__logo" src="Assets/afbeeldingen/snackbarlogo.png"  alt="logo__link__to__home"></a>
-                    <ul class="ul1"><a href="#home">home</a></ul>
-                    <ul><a href="#about">about us</a></ul>
-                    <ul><a href="#menu">menu</a></ul>
-                    <ul><a href="#form">formulier</a></ul>           
-                    <ul><a href="#contact">contact us</a></ul>
-                  </li>
-                </nav>
-              </div>
-            </header>
+            
             <div class="burger__pic">
 
             </div>
@@ -56,7 +46,7 @@
 
         </div>
 
-        <div class="container__werken__bigsnack">
+        <div class="container__werken__bigsnack" id="werken">
           
         </div>
 
