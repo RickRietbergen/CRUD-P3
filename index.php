42359ca01dcf8f61d7ad6211e49c9f115e8acf22
RickRietbergen <rbps3.2016@gmail.com>
2022-04-02 00:08:35 +0200

    idk

--- index.php
+++ index.php
@@ -19,6 +19,11 @@
     <?php
       include_once("header.php");
       require_once("connect.php");
+      //connect to database
+      $sql = "SELECT * FROM menu";
+      $stmt = $connect->prepare(sql);
+      $stmt = execute();
+      $result = $stmt->fetchALL();
     ?>
     <main>
       <div class="container__homepage" id="home">
