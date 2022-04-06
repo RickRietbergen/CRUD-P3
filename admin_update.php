35ce4e2d50783faac4adf0984a1f438452e18fd6
RickRietbergen <rbps3.2016@gmail.com>
2022-04-06 12:15:46 +0200

    updateProduct, form info inserted, form info send to database

--- admin_update.php
+++ admin_update.php
@@ -9,10 +9,38 @@
     <title>Edit row</title>
 </head>
 <body>
+       <?php
+      //include header, connection to database, footer.
+      require_once("includes/connect.php");
+      /**
+      * @var PDO $connect
+      */
+
+      //connect to database
+      $sql = "SELECT * FROM menu
+      WHERE ID =:id";
+      $stmt = $connect->prepare($sql);
+      $stmt->bindParam(":id", $_GET['ID']);
+      $stmt-> execute();
+      $result = $stmt->fetchALL();
+    ?>
+
     <main class="admin-update">
         <h2>Edit Row</h2>
-        <form action="">
-`            <label for="">Product Name:</label>
+        <form action="PHP/updateProduct.php" method="post">
+            <label for="">ID:</label>
+            <input
+                class="edit-product-all"
+                id="edit-product-id"
+                type="text"
+                name="ID"
+                maxlength="10"
+                placeholder="ID:"
+                value="<?php echo $result[0]["ID"]?>"
+                readonly
+            />
+
+            <label for="">Product Name:</label>
             <input
                 class="edit-product-all"
                 id="edit-product-name"
@@ -20,16 +48,19 @@
                 name="fullname"
                 maxlength="25"
                 placeholder="First Name"
+                value="<?php echo $result[0]["product_name"]?>"
+                required
+                
             />
-
             <label for="">Picture URL:</label>
             <input
                 class="edit-product-all"            
                 id="edit-picture-url"
                 type="text"
-                name="fullname"
+                name="pictureURL"
                 maxlength="25"
                 placeholder="First Name"
+                value="<?php echo $result[0]["picture"]?>"
             />
 
             <label for="">Prijs:</label>
@@ -37,9 +68,11 @@
                 class="edit-product-all"
                 id="edit-prijs"
                 type="text"
-                name="fullname"
+                name="prijs"
                 maxlength="25"
                 placeholder="First Name"
+                value="<?php echo $result[0]["prijs"]?>"
+                required
             />
             
             <label for="">Voorraad:</label>
@@ -47,9 +80,11 @@
                 class="edit-product-all"
                 id="edit-voorraad"
                 type="text"
-                name="fullname"
+                name="voorraad"
                 maxlength="25"
                 placeholder="First Name"
+                value="<?php echo $result[0]["voorraad"]?>"
+                required
             />
 
             <label for="">Categorie:</label>
@@ -57,18 +92,15 @@
                 class="edit-product-all"
                 id="edit-categorie"
                 type="text"
-                name="fullname"
+                name="categorie"
                 maxlength="25"
                 placeholder="First Name"
+                value="<?php echo $result[0]["categorie"]?>"
+                required
             />
-            <input id="submit" type="button" class="" value="Submit" />
+            <input id="submit" type="submit" class="" value="Submit" name="submit" />
 
         </form>
     </main>
 </body>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="crossorigin="anonymous">
-    </script>
-    <script src="JS/admin_edit.js">
-    </script>
 </html>
